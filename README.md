bb672471c50adaac4bc465cf3d361f3f8eee51d0
Kim YoungJun <dev.youngjun@gmail.com>
2023-02-16 21:02:14 +0900

    설문 데이터 위치 변경, README 보충

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 ## Simple survey
 
 javascript를 기반으로 한 간단한 설문 코드 입니다.
+
+설문 인스턴스를 생성 후 onComplete 콜백함수를 등록하시면 됩니다.
+
+설문 생성시 들어가는 인자에서 선택지 이름(optionLabels)과, 선택지 값(optionValues)의 길이는 동일해야 합니다.
+
+---
+
+const survey = new Survey(설문을 그릴 컨테이너의 ID, 문항 데이터, 선택지 이름, 선택지 값);
+
+survey.setOnComplete((answer) => {...});
+
+survey.setOnChangeQuestion((idx) => {...});
--- survey_page.js
+++ survey_page.js
@@ -3,6 +3,51 @@ const d = document;
 /**
  * Initialize
  */
+const optionLabels = ['Option 1', 'Option 2', 'Option 3', 'Option 4', 'Option 5'];
+const optionValues = [1, 2, 3, 4, 5];
+const surveyData = [
+  {
+    index: 1,
+    question: 'Content of question 1',
+  },
+  {
+    index: 2,
+    question: 'Content of question 2',
+  },
+  {
+    index: 3,
+    question: 'Content of question 3',
+  },
+  {
+    index: 4,
+    question: 'Content of question 4',
+  },
+  {
+    index: 5,
+    question: 'Content of question 5',
+  },
+  {
+    index: 6,
+    question: 'Content of question 6',
+  },
+  {
+    index: 7,
+    question: 'Content of question 7',
+  },
+  {
+    index: 8,
+    question: 'Content of question 8',
+  },
+  {
+    index: 9,
+    question: 'Content of question 9',
+  },
+  {
+    index: 10,
+    question: 'Content of question 10',
+  },
+];
+
 const survey = new Survey('surveyContainer', surveyData, optionLabels, optionValues);
 survey.setOnComplete((answer) => {
   alert(answer);
